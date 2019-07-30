2410d197a7c943e151a3b5cc1217ebe4f40bb248
Miguel Lopes <miguel.lopes@miniclip.com>
2019-07-30 22:49:58 +0100

    Init commit

---
+++ Cubenator/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.AddForce(0,0,2000 * Time.deltaTime);
+    }
+}
