cfb67cd28529f0c3420939823573d11395febb7f
Miguel Lopes <miguel.lopes@miniclip.com>
2019-07-31 22:30:14 +0100

    add score, more levels

---
+++ Cubenator/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+        
+    }
+}
